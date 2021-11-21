f1d4d0c9b8906e674e7528c0798cda5ca0c58293
Andrey Yarosh <andreyarosh7@gmail.com>
2021-11-21 21:06:50 +0300

    add changes to mysql and controllers

--- src/test/java/by/itstep/controllers/impl/UserControllerImplTest.java
+++ src/test/java/by/itstep/controllers/impl/UserControllerImplTest.java
@@ -2,9 +2,6 @@ package by.itstep.controllers.impl;
 
 import by.itstep.models.User;
 import org.junit.Test;
-
-import java.util.Optional;
-
 import static org.junit.Assert.*;
 
 public class UserControllerImplTest {
